952c4c521ef0ff2d9085f9155676935f4725948b
Dale S Hendrickson <dale@dshendrickson.com>
2016-12-07 15:52:59 -0700

    WIP SAME PAGE LINK

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('reminders', () => {
-    this.route('individual-reminder')
+    this.route('individual-reminder', {path: '/reminders/:id'})
   });
 });
 
