3ffb947fe897e0a20a9d388508c2e3242b47648b
Dale S Hendrickson <dale@dshendrickson.com>
2016-12-13 16:24:00 -0700

    Minor changes to the edit route

--- app/routes/reminders/edit.js
+++ app/routes/reminders/edit.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    this.get('store').findRecord('reminder', params.reminder_id)
+    this.get('store').findRecord('reminder', params.reminder_id);
   }
 });
